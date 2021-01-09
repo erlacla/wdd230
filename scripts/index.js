410e80476fda7914e8a9348122518b91eaff4ecc
erlacla/wdd230 <erlacla@msn.com>
2021-01-08 20:52:11 -0700

    assignment portal

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+let string = new Date(document.lastModified);
+//const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').innerHTML = string;
