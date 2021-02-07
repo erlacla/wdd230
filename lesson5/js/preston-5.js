fbe3b88f28096a71870b7d1090d2d52ca539c862
erlacla/wdd230 <erlacla@msn.com>
2021-02-06 18:47:04 -0700

    more fixes

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -9,13 +9,15 @@ function toggleMenu() {
   document.getElementById("nav").classList.toggle("hide");
 }
 
-today.getDate() == 5 ? (pancake.style.display = "block") : pancake.style.display === "none";
-
+today.getDate() == 5
+  ? (pancake.style.display = "block")
+  : pancake.style.display === "none";
 
 if (!localStorage.visited) {
-    localStorage.setItem('siteVisitCount', site_visit + 1);
-    // set update to DOM element
-    document.getElementById("result").innerHTML = 'Welcome, You visited this page '+ localStorage.siteVisitCount + 'time';
-    // need to send updated value to database 
-    localStorage.visited = true;
-  }
+  localStorage.setItem("siteVisitCount", site_visit + 1);
+  // set update to DOM element
+  document.getElementById("result").innerHTML =
+    "Welcome, You visited this page " + localStorage.siteVisitCount + "time";
+  // need to send updated value to database
+  localStorage.visited = true;
+}
