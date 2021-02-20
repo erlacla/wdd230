b93294a327ef9a3a6578180a09b24199c6c35967
erlacla/wdd230 <erlacla@msn.com>
2021-02-19 23:04:19 -0700

    only box shadow on large view

--- lesson6/js/preston-6.js
+++ lesson6/js/preston-6.js
@@ -26,10 +26,10 @@ let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //when the img is 50px from the window bottom, run function
 const imgOptions = {
-    threshold: 0, 
-    rootMargin: "0px 0px 50px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
 };
-//change from placeholder (source) to actual image (data source) 
+//change from placeholder (source) to actual image (data source)
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
   image.onload = () => {
@@ -37,24 +37,22 @@ const loadImages = (image) => {
   };
 };
 //only remove switch from src to data-src when the img is intersecting in window
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
     });
-    
-    //run function on each image
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+  });
 
-
+  //run function on each image
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
