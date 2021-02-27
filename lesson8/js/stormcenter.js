111103d80d917b48fc21b52523865108731010f4
erlacla/wdd230 <erlacla@msn.com>
2021-02-27 15:14:59 -0700

    week 8 stormcenter page

---
+++ lesson8/js/stormcenter.js
@@ -0,0 +1,58 @@
+const today = new Date();
+let currentDay = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  today
+);
+let date = document.querySelector("#currentdate");
+date.textContent = currentDay;
+
+function toggleMenu() {
+  document.querySelector("#nav").classList.toggle("hide");
+}
+
+let visit = localStorage.getItem("on_load_counter");
+if (visit === null) {
+  visit = 0;
+}
+visit++;
+localStorage.setItem("on_load_counter", visit);
+document.querySelector("#visits").innerHTML = visit;
+
+//first find all the data-source images with query selector
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//when the img is 50px from the window bottom, run function
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
+//change from placeholder (source) to actual image (data source)
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+//only remove switch from src to data-src when the img is intersecting in window
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  //run function on each image
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
