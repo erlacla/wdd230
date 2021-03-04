097846998b2fc8e399a53a67ccbd22bf83e12558
erlacla/wdd230 <erlacla@msn.com>
2021-03-03 20:17:41 -0700

    fixes

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,7 +5,6 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
     const prophets = jsonObject["prophets"];
 
     for (let i = 0; i < prophets.length; i++) {
