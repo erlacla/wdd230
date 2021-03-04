4be8ce8ba9bddecab272ad92d9ebeaa76ca2ae12
erlacla/wdd230 <erlacla@msn.com>
2021-03-04 16:20:02 -0700

    table fix

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -17,7 +17,7 @@ const myTowns = [];
 myTowns.push(...preston, ...sodasprings, ...fishhaven);
 console.log(myTowns);
 
-myTowns.forEach(town => {
+myTowns.forEach((town) => {
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
@@ -41,3 +41,71 @@ myTowns.forEach(town => {
   
 });
   });
+
+
+
+
+
+
+
+
+  
+
+  const today = new Date();
+let currentDay = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  today
+);
+let date = document.querySelector("#currentdate");
+date.textContent = currentDay;
+
+function toggleMenu() {
+  document.querySelector("#nav").classList.toggle("hide");
+}
+
+if (today.getDay() == 5) {
+  document.getElementById("pancake").style.display = "block";
+}
+
+let visit = localStorage.getItem("on_load_counter");
+if (visit === null) {
+  visit = 0;
+}
+visit++;
+localStorage.setItem("on_load_counter", visit);
+document.querySelector("#visits").innerHTML = visit;
+
+//first find all the data-source images with query selector
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//when the img is 50px from the window bottom, run function
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
+//change from placeholder (source) to actual image (data source)
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+//only remove switch from src to data-src when the img is intersecting in window
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  //run function on each image
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
