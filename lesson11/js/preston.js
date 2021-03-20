dfed34f6f6d491d6546e11aaa8d3c6933a7d5d6d
erlacla/wdd230 <erlacla@msn.com>
2021-03-20 16:56:58 -0700

    fixes

--- lesson11/js/preston.js
+++ lesson11/js/preston.js
@@ -1,3 +1,7 @@
+function toggleMenu() {
+  document.querySelector("#nav").classList.toggle("hide");
+}
+
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=193243ab32ebe2eba039565713596c16";
 fetch(apiURL)
@@ -47,8 +51,8 @@ fetch(forecast)
         dayOfWeek[d.getDay()];
       document.getElementById(`forecast${day + 1}`).innerHTML =
         Math.round(x.main.temp) + "&#176;F";
-      const src =
-        "https://openweathermap.org/img/wn/" + x.weather[0].icon + "@2x.png";
+        const src =
+        `https://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png`;
       const alt = x.weather[0].description;
       document.getElementById(`icon${day + 1}`).setAttribute("src", src);
       document
@@ -65,9 +69,7 @@ let currentDay = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 let date = document.querySelector("#currentdate");
 date.textContent = currentDay;
 
-function toggleMenu() {
-  document.querySelector("#nav").classList.toggle("hide");
-}
+
 
 if (today.getDay() == 5) {
   document.getElementById("pancake").style.display = "block";
@@ -81,41 +83,6 @@ visit++;
 localStorage.setItem("on_load_counter", visit);
 document.querySelector("#visits").innerHTML = visit;
 
-//first find all the data-source images with query selector
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-
-//when the img is 50px from the window bottom, run function
-const imgOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 50px 0px",
-};
-//change from placeholder (source) to actual image (data source)
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
-};
-//only remove switch from src to data-src when the img is intersecting in window
-if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-
-  //run function on each image
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
 
 const requestURL =
   "https://byui-cit230.github.io/weather/data/towndata.json";
